90a7c31e8c06a8b7ddb05f1faf9879ac6b6f72ab
Gleison <gleisonmorais06@gmail.com>
2026-04-16 17:49:05 -0400

    refactor: state to states and sidebar position

--- lua/zenvoy/states.lua
+++ lua/zenvoy/states.lua
@@ -7,8 +7,11 @@ M.current_page = 1
 M.current_envelope_page = 0
 M.sidebar = nil
 M.sidebar_popup = nil
-M.is_open = false
+M.sidebar_navigation = "left"
+M.sidebar_email = "right"
+M.is_open = true
 M.emails_list = {}
 M.command = "Zenvoy"
 
 return M
+
