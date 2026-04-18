e8ddeeadb21c312450a2c95803624cef53716792
Gleison <gleisonmorais06@gmail.com>
2026-04-18 14:35:06 -0400

    add: layout folders and emails columns

--- lua/zenvoy/ui/layout.lua
+++ lua/zenvoy/ui/layout.lua
@@ -1,32 +1,72 @@
 local M = {}
 
--- local state = require("zenvoy.states")
+local state = require("zenvoy.states")
 
 local Popup = require("nui.popup")
--- local Layout = require("nui.layout")
+local Layout = require("nui.layout")
+local Text = require("nui.text")
+
 
 function M.create()
-  local sidebar = Popup ({
-    position = "50%",
-    size = {
-      width = 150,
-      height = 40,
-    },
+  local aside = Popup ({
     enter = false,
     focusable = true,
+    relative = "editor",
     border = {
       style = "rounded",
       text = {
-        top = " Folders ",
+        top = Text(" Folders ", "FloatBorder"),
         top_align = "center",
       },
     },
     win_options = {
       cursorline = true
     },
+    buf_options = {
+      modifiable = false,
+      readonly = true,
+    },
     zindex = 49,
   })
-  sidebar:mount()
+
+  local email = Popup ({
+    enter = false,
+    focusable = true,
+    relative = "editor",
+    border = {
+      style = "rounded",
+      text = {
+        top = Text(" Emails ", "FloatBorder"),
+        top_align = "center",
+      },
+    },
+    win_options = {
+      cursorline = true
+    },
+    buf_options = {
+      modifiable = false,
+      readonly = true,
+    },
+    zindex = 49,
+  })
+
+  local aside_pct = state.aside or 25
+  local email_pct = 100 - aside_pct
+
+  local main = Layout({
+      position = "50%",
+      relative = "editor",
+      size = {
+        width = "90%",
+        height = "80%",
+      },
+    },
+    Layout.Box({
+      Layout.Box(aside, { size = tostring(aside_pct) .. "%" }),
+      Layout.Box(email, { size = tostring(email_pct) .. "%" })
+    }, { dir = "row" })
+  )
+  main:mount()
 end
 
 return M
